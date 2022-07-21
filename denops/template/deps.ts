09da528eab7d98da0cdaa64937fce4e57f60c005
skanehira <skanehira@users.noreply.github.com>
2022-07-21 00:22:57 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-template.vim/denops-template.vim/denops/template/deps.ts
    [1/4] Looking for releases: https://deno.land/x/denops_std@v3.3.2/mod.ts
    [1/4] Using latest: https://deno.land/x/denops_std@v3.3.2/mod.ts
    [2/4] Looking for releases: https://deno.land/x/denops_std@v3.3.2/variable/mod.ts
    [2/4] Using latest: https://deno.land/x/denops_std@v3.3.2/variable/mod.ts
    [3/4] Looking for releases: https://deno.land/x/unknownutil@v2.0.0/mod.ts
    [3/4] Using latest: https://deno.land/x/unknownutil@v2.0.0/mod.ts
    [4/4] Looking for releases: https://deno.land/std@0.148.0/testing/asserts.ts
    [4/4] Attempting update: https://deno.land/std@0.148.0/testing/asserts.ts -> 0.149.0
    [4/4] Update successful: https://deno.land/std@0.148.0/testing/asserts.ts -> 0.149.0
    
    Already latest version:
    https://deno.land/x/denops_std@v3.3.2/mod.ts == v3.3.2
    https://deno.land/x/denops_std@v3.3.2/variable/mod.ts == v3.3.2
    https://deno.land/x/unknownutil@v2.0.0/mod.ts == v2.0.0
    
    Successfully updated:
    https://deno.land/std@0.148.0/testing/asserts.ts 0.148.0 -> 0.149.0

--- denops/template/deps.ts
+++ denops/template/deps.ts
@@ -1,4 +1,4 @@
 export * from "https://deno.land/x/denops_std@v3.3.2/mod.ts";
 export * from "https://deno.land/x/denops_std@v3.3.2/variable/mod.ts";
 export * from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
-export * from "https://deno.land/std@0.148.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.149.0/testing/asserts.ts";
